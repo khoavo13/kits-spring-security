43419fa2eced6f66d049002ca18271b7200b893e
khoavo13 <vandangkhoa.vo@gmail.com>
2024-09-18 14:23:27 +0700

    first commit

---
+++ src/main/java/com/example/security/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.example.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/category")
+public class CategoryController {
+    @GetMapping("")
+    public String index() {
+        return "list category";
+    }
+
+    @GetMapping("/new")
+    public String login() {
+        return "login category";
+    }
+}
